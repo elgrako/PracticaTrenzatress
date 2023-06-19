e7d1ac38749dc94a48e9d77dc5a92bae8c7d4b35
elgrako <103579462+elgrako@users.noreply.github.com>
2023-06-19 19:46:04 +0200

    Update botonmostrar.js

--- botonmostrar.js
+++ botonmostrar.js
@@ -1,7 +1,12 @@
 const mostrarEspecificaciones1 = document.getElementById('mostrarEspecificaciones1');
 const especificaciones1 = document.getElementById('especificaciones1');
 
-mostrarEspecificaciones1.addEventListener('click', () => {
-  especificaciones1.style.display = 'block';
-});
-
+mostrarEspecificaciones1.onclick = function() {
+  if (especificaciones1.style.display === 'none') {
+    especificaciones1.style.display = 'block';
+    especificaciones1.style.marginLeft = '200px';
+  } else {
+    especificaciones1.style.display = 'none';
+    especificaciones1.style.marginLeft = '0px';
+  }
+};
